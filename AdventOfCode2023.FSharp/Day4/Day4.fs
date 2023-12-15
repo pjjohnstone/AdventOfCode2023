a511b5562e1772408ac95608babe081525b152c0
Patrick Johnstone <patrick.johnstone@spektrix.com>
2023-12-15 10:39:31 +0000

    Solve part 1 day4

---
+++ AdventOfCode2023.FSharp/Day4/Day4.fs
@@ -0,0 +1,45 @@
+﻿module Day4
+
+let stripCruft (str: string) =
+  str.Split(' ')
+  |> Array.filter (fun x -> x <> "")
+
+let getNumbers array =
+  array
+  |> Array.toList
+  |> List.map stringToInt
+
+let card (line: string) =
+  let halves = 
+    line.Split('|') 
+    |> Array.toList
+  let winning = 
+    stripCruft (halves.Head.Split(':')[1])
+    |> getNumbers
+  let own = 
+    stripCruft halves[1]
+    |> getNumbers
+  (winning,own)
+
+let cards lines =
+  lines
+  |> List.map card
+
+let scoringNumbers (card: int list * int list) =
+  let (winning,own) = card
+  Set.intersect (Set.ofList winning) (Set.ofList own)
+  |> Set.toList
+
+let calculateScore (scoringNumbers: int list) =
+  match scoringNumbers.Length with
+  | 0 -> 0.0
+  | 1 -> 1.0
+  | _ -> 2.0 ** (float (scoringNumbers.Length - 1))
+
+let solve1 lines =
+  lines
+  |> cards
+  |> List.map scoringNumbers
+  |> List.map calculateScore
+  |> List.sum
+  |> int
--- AdventOfCode2023.FSharp/Program.fs
+++ AdventOfCode2023.FSharp/Program.fs
@@ -1,4 +1,4 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
-let lines = getLines "Day1/input.txt"
-printfn "Total of calibrations: %i" (Day1.solve1 lines)
+let lines = getLines "Day4/input.txt"
+printfn "Scores: %A" (Day4.solve1 lines)
