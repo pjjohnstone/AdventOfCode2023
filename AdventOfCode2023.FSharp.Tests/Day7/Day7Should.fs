e22563cf6b15b6b97a6c7567397e3696b731bf62
Patrick Johnstone <patrick.johnstone@spektrix.com>
2024-01-12 10:52:55 +0000

    Implement values of all cards

--- AdventOfCode2023.FSharp.Tests/Day7/Day7Should.fs
+++ AdventOfCode2023.FSharp.Tests/Day7/Day7Should.fs
@@ -5,6 +5,17 @@ open Day7
 
 let valueCases = [
   TestCaseData('A').Returns(13)
+  TestCaseData('K').Returns(12)
+  TestCaseData('Q').Returns(11)
+  TestCaseData('J').Returns(10)
+  TestCaseData('T').Returns(9)
+  TestCaseData('9').Returns(8)
+  TestCaseData('8').Returns(7)
+  TestCaseData('7').Returns(6)
+  TestCaseData('6').Returns(5)
+  TestCaseData('5').Returns(4)
+  TestCaseData('4').Returns(3)
+  TestCaseData('3').Returns(2)
   TestCaseData('2').Returns(1)
 ]
 
