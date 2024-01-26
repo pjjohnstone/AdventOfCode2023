19e5a62ef836eaf11fb8ac21fe87c1e2bd83fae7
Patrick Johnstone <patrick.johnstone@spektrix.com>
2024-01-26 13:12:41 +0000

    Refactor tests

--- AdventOfCode2023.FSharp.Tests/Day7/Day7Should.fs
+++ AdventOfCode2023.FSharp.Tests/Day7/Day7Should.fs
@@ -92,23 +92,24 @@ let handOrderCases = [
     ("KK677", 28)
     ("KTJJT", 220)
     ("QQQJA", 483)
-  ])
-]
-
-[<TestCaseSource("handOrderCases")>]
-let ``Returns list of hands and bids in ranked order`` s =
-  let expected = [
+  ],[
     ("QQQJA", 483)
     ("T55J5", 684)
     ("KK677", 28)
     ("KTJJT", 220)
     ("32T3K", 765)
-  ]
-  Assert.That(handOrder s, Is.EquivalentTo expected)
+  ])
+]
+
+[<TestCaseSource("handOrderCases")>]
+let ``Returns list of hands and bids in ranked order`` s =
+  let (input, expected) = s
+  Assert.That(handOrder input, Is.EquivalentTo expected)
 
 [<TestCaseSource("handOrderCases")>]
 let ``Returns total value of hands`` s =
-  Assert.That(calculateValue s, Is.EqualTo 6440)
+  let (input, _) = s
+  Assert.That(calculateValue input, Is.EqualTo 6440)
 
 [<Test>]
 let ``Parse an input line`` =
