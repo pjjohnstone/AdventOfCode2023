b0dd24f04db2f338dca132bf87d6b84dbab31219
Patrick Johnstone <patrick.johnstone@spektrix.com>
2024-11-29 12:04:25 +0000

    Start Day8

---
+++ AdventOfCode2023.FSharp.Tests/Day8/Day8Should.fs
@@ -0,0 +1,14 @@
+﻿module AdventOfCode2023.FSharp.Tests.Day8.Day8Should
+
+open NUnit.Framework
+open AdventOfCode2023.FSharp.Day8.Day8
+
+let instructionCases = [
+  TestCaseData(Left, {Label = "AAA"; Left = "BBB"; Right = "CCC"} ).Returns("BBB")
+  TestCaseData(Right, {Label = "AAA"; Left = "BBB"; Right = "CCC"} ).Returns("CCC")
+] 
+
+[<TestCaseSource("instructionCases")>]
+let ``Pick correct path based on instruction`` input =
+  let (instruction, paths) = input
+  nextNode instruction paths
---
+++ AdventOfCode2023.FSharp/Day8/Day8.fs
@@ -0,0 +1,16 @@
+﻿module AdventOfCode2023.FSharp.Day8.Day8
+
+type Node = {
+  Label: string
+  Left: string
+  Right: string
+}
+
+type Instruction =
+  | Left
+  | Right
+
+let nextNode instruction node:string =
+  match instruction with
+  | Left -> node.Left
+  | Right -> node.Right
